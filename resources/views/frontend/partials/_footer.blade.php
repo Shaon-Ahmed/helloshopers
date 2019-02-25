2eafa37fd6d64202f03da7235525abc841fc30b8
Shaon-Ahmed <shahnoor.shaon@gmail.com>
2019-02-25 16:43:39 +0600

    master layout completed

---
+++ resources/views/frontend/partials/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="text-muted">
+    <div class="container">
+        <p class="float-right">
+            <a href="#">Back to top</a>
+        </p>
+        <p> &copy; {{ config('app.name') }} 2019</p>
+        <p>New to {{ config('app.name') }}? <a href="../../">Signup now</a> or call +8801675557079 </p>
+    </div>
+</footer>
